43bd1bdbac397177ed62bd21613fee485cd8ced1
522622504 <522622504@qq.com>
2025-08-21 17:15:24 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -4,35 +4,22 @@ const TEMPLATE_CONFIG = [
     id: '0001',
     title: '亲爱的, 早上好',
     desc: `
-      **{{date.DATA}}**
-      
-      下个休息日：{{wx_holidaytts_{index}.DATA}}
-      
-      ---
-      
+      **{{date.DATA}}**   
       城市：{{city.DATA}}
-      
       天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}} 
       今天是我们在一起的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
-      ---
+      {{one_talk.DATA}} -- {{talk_from.DATA}}
+
+      {{earthy_love_words.DATA}}
       
       {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      {{note_en.DATA}} 
+      {{note_ch.DATA}}
     `
   },
   {
